3d79d2406140a2a854eb3bd4014adad0e4b08a93
emias11 <mia.stapleton1@gmail.com>
2021-03-06 14:09:38 +0000

    made flask app

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, world")
+from flask import render_template, Flask, jsonify, redirect, request, url_for, send_file
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def load_app():
+	return render_template("homehtml.html")
+
+
+def main():
+	app.run(debug=True)
+
+
+if __name__ == "__main__":
+	main()
